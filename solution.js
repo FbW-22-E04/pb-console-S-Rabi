b3d34575bcbbdb08014f1698d1111d05b6ff3e02
S Rabi <rabi.sanaye@yahoo.com>
2022-06-29 13:58:30 +0200

    The first js exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,45 @@
 // write your code in this file
+const exercise = "variable";
+console.log("This is exercise", exercise);
+/** 
+const firstName = "Rabi";
+const lastName = "Sanaye";
+const age = 31;
+const city = "Reinfeld";
+const job = "Frontend-Developer";
+console.log(
+  "my name is",
+  firstName,
+  "and my last name is",
+  lastName,
+  "I am",
+  age,
+  "my city is",
+  city,
+  "and I am",
+  job
+);
+
+console.log(lastName);
+console.log(age);
+console.log(job);
+console.log(city);
+console.log(firstName);
+*/
+const firstName = "John";
+const lastName = "Smit";
+const age = 43;
+const city = "Berlin";
+const job = "Teacher";
+console.log(
+  "my name is",
+  firstName,
+  "and my last name is",
+  lastName,
+  "I am",
+  age,
+  "my city is",
+  city,
+  "and I am",
+  job
+);
